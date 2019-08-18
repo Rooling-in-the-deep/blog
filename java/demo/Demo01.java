885368303836f39cb65d421fa7afb45617f26fc8
pan.ruihui@bvaluate.com <pan.ruihui@bvaluate.com>
2019-08-18 23:29:16 +0800

    ==和equal的区别

--- java/demo/Demo01.java
+++ java/demo/Demo01.java
@@ -3,5 +3,14 @@ package demo;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("Hello World~");
+
+        String a="ab";
+        String b = new String("ab");
+        System.out.println(a.equals(b));
+        System.out.println(b.intern().equals(b));
+        System.out.println(a==b);//一个字符串缓冲区，一个内存栈。
+        System.out.println(b.intern()==a);//b调用intern函数后，如果字符串缓冲区没有对应的值，会自行创建出来，
+//                                                        如果有对应的值，则返回的对象地址，就是字符串缓冲区中对象的地址
+
     }
 }
